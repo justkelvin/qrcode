d10f53cba7e48e8ff85d178a60575098f665615b
n07f0und <muia.muinde@s.karu.ac.ke>
2021-09-30 21:22:46 +0300

    Code cleaned

--- qrgenerator.py
+++ qrgenerator.py
@@ -7,20 +7,20 @@ import traceback, sys
 from colorama import Fore, Style
 from banner import banner
 
-exit_text = '\n\n[!] Closing... Goodbye. (^-^)\n'
 def main():
     try:
         def generate_qr():
             """Use pyqrcode to convert text and generate a qrcode"""
-            raw_text = input(Fore.BLUE + '\n[>] Enter text to convert: ' + Style.RESET_ALL)
+            raw_text = input(Fore.BLUE + '[>] Enter text to convert: ' + Style.RESET_ALL)
             qr_code = pyqrcode.create(raw_text)
             print(qr_code.terminal())
-            qr_code.png('qr_images/qr.png', scale=10)
+            qr_code.png(f'qr_images/{raw_text}.png', scale=10)
             
         print(banner())
         generate_qr()
             
     except KeyboardInterrupt:
+        exit_text = '\n\n[!] Closing... Goodbye. (^-^)\n'
         print(Fore.RED + exit_text)
     except Exception:
         traceback.print_exc(file=sys.stdout)
