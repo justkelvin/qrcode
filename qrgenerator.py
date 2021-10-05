a677b050733c667a16742bb1b22570b15daed0a6
n07f0und <muia.muinde@s.karu.ac.ke>
2021-10-05 21:09:03 +0300

    Improved overal script functionality and fixed bug
    The script can now accept arguments
    - Added output arg to specify a custom image name
    - Added the functionality to run the script with -i flag to specify your text string
    - Fixed all the bugs in arguments parsing part
    - Removed obsolete code lines

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,29 +1,53 @@
 #!/usr/bin/env python3
 
-# Imports
-
 import pyqrcode
+import argparse
 import traceback, sys
 from colorama import Fore, Style
 from banner import banner
 
+def generate_qr(raw_text='', file_name=''):
+    """Use pyqrcode to convert text and generate a qrcode"""        
+    qr_code = pyqrcode.create(raw_text)
+    print(qr_code.terminal())
+    qr_code.png(f'qr_images/{file_name}.png', scale=10)
+
 def main():
+    parser = argparse.ArgumentParser(description='Creates a qrcode and a png file of the qrcode')
+    parser.add_argument('-i', '--input', help='Your input text string to be converted')
+    parser.add_argument('-o', '--output', help='Custom output png file.')
+    args = parser.parse_args()
+        
     try:
-        def generate_qr():
-            """Use pyqrcode to convert text and generate a qrcode"""
-            raw_text = input(Fore.BLUE + '[>] Enter text to convert: ' + Style.RESET_ALL)
-            qr_code = pyqrcode.create(raw_text)
-            print(qr_code.terminal())
-            qr_code.png(f'qr_images/{raw_text}.png', scale=10)
+        if args.input and args.output:
+            raw_text = args.input
+            file_name = args.output
+            print(banner())
+            generate_qr(raw_text, file_name)
             
-        print(banner())
-        generate_qr()
+        elif args.input or args.output:
+            raw_text = args.input
+            file_name = args.input
+            print(banner())
+            generate_qr(raw_text, file_name)
+
+        else:
+            print(banner())
+            raw_text = input(Fore.BLUE + '[>] Enter text to convert: ' + Style.RESET_ALL)
+            file_name = raw_text
+            generate_qr(raw_text, file_name)
             
     except KeyboardInterrupt:
         exit_text = '\n\n[!] Closing... Goodbye. (^-^)\n'
         print(Fore.RED + exit_text)
+        
     except Exception:
         traceback.print_exc(file=sys.stdout)
+        
+    except ModuleNotFoundError:
+        print(Fore.RED + '[!] You have missing dependancies...')
+        print(Fore.RED + '[-] Try ' + Fore.GREEN + 'pip3 install -r requirements.txt')
+        
     sys.exit(0)
     
 if __name__ == '__main__':
