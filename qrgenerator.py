09708cbc8bffd0c99bd61fbdf5858593cf17269a
n07f0und <muia.muinde@s.karu.ac.ke>
2021-10-07 12:06:12 +0300

    Fixed bugs

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
-import pyqrcode
-import argparse
-import traceback, sys
-from colorama import Fore, Style
-from banner import banner
+try:
+    import pyqrcode
+    import argparse
+    import traceback, sys
+    from colorama import Fore, Style
+    from banner import banner
+except ModuleNotFoundError:
+    print(Fore.RED + '[!] You have missing dependancies...')
+    print(Fore.RED + '[-] Try ' + Fore.GREEN + 'pip3 install -r requirements.txt')    
 
 def generate_qr(raw_text='', file_name=''):
-    """Use pyqrcode to convert text and generate a qrcode"""        
+    """Use pyqrcode to convert text and generate a qrcode"""
     qr_code = pyqrcode.create(raw_text)
     print(qr_code.terminal())
     qr_code.png(f'qr_images/{file_name}.png', scale=10)
@@ -17,14 +21,14 @@ def main():
     parser.add_argument('-i', '--input', help='Your input text string to be converted')
     parser.add_argument('-o', '--output', help='Custom output png file.')
     args = parser.parse_args()
-        
+
     try:
         if args.input and args.output:
             raw_text = args.input
             file_name = args.output
             print(banner())
             generate_qr(raw_text, file_name)
-            
+
         elif args.input or args.output:
             raw_text = args.input
             file_name = args.input
@@ -36,19 +40,15 @@ def main():
             raw_text = input(Fore.BLUE + '[>] Enter text to convert: ' + Style.RESET_ALL)
             file_name = raw_text
             generate_qr(raw_text, file_name)
-            
+
     except KeyboardInterrupt:
         exit_text = '\n\n[!] Closing... Goodbye. (^-^)\n'
         print(Fore.RED + exit_text)
-        
+
     except Exception:
         traceback.print_exc(file=sys.stdout)
-        
-    except ModuleNotFoundError:
-        print(Fore.RED + '[!] You have missing dependancies...')
-        print(Fore.RED + '[-] Try ' + Fore.GREEN + 'pip3 install -r requirements.txt')
 
     sys.exit(0)
-    
+
 if __name__ == '__main__':
     main()
