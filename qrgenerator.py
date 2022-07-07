97f8eb7ad3d1d0f63a5247cf9fb279c649366987
justkelvin <muia.muinde@s.karu.ac.ke>
2022-07-07 14:45:56 +0300

    feat: add parse option
    decode will be used to call the decode pyzbar function and pass the file
    name as a parameter

--- qrgenerator.py
+++ qrgenerator.py
@@ -3,6 +3,9 @@
 from os import error
 import argparse
 import traceback, sys, os
+from isort import file
+
+from yaml import parse
 from banner import banner
 from datetime import datetime
 try:
@@ -22,12 +25,13 @@ def generate_qr(raw_text='', file_name=''):
     qr_code.png(f'qrcodes/{file_name}.png', scale=10)
     print(Fore.GREEN + "\nYour image has been saved at " + Fore.CYAN + f"qrcodes/{file_name}.png")
 
-def read_qr():
+def read_qr(file_name):
     """Read a qrcode image and determine its content"""
-    img = Image.open()
+    img = Image.open(file_name)
     qr_data = decode(img)
-    print(Fore.GREEN + "[-] Scanning...\n")
-    print(Fore.WHITE + "[»] " + qr_data)
+    # print(Fore.GREEN + "[-] Scanning...\n")
+    # print(Fore.WHITE + "[»] " + qr_data)
+    print(qr_data)
 
 def path_chk():
     path = "qrcodes"
@@ -41,6 +45,7 @@ def main():
     parser = argparse.ArgumentParser(description='Creates a qrcode and a png file of the qrcode')
     parser.add_argument('-i', '--input', help='Your input text string to be converted')
     parser.add_argument('-o', '--output', help='Custom output png file.')
+    parser.add_argument('-d', '--decode', help='Decode a qr code image.')
     args = parser.parse_args()
 
     try:
@@ -55,6 +60,11 @@ def main():
             print(Fore.RED + err_msg + Fore.RESET + banner())
             os.system("./qrgenerator.py -h")
             sys.exit(1)
+
+        elif args.decode:
+            file_name = args.decode
+            print(banner())
+            read_qr(file_name)
         
         elif args.input and args.output:
             raw_text = args.input
